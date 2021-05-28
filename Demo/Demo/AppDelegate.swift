08bbaadceb4606f97dfcc5b139c6fcc0f434832b
Rustam Nurgaliev <Rustik6030@gmail.com>
2021-05-28 22:36:51 +0300

    Updated default configuration

--- Demo/Demo/AppDelegate.swift
+++ Demo/Demo/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let configs = MLKConfiguration(
-            isEnable: false,
+            isEnable: true,
             notificationType: [.push, .console],
             messageTrigger: 2
         )
